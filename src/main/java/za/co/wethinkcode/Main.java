8fe4011f62489284b8c4953c86c415fe3d83e7d9
GeePaw Hill <geepawhill@geepawhill.org>
2025-02-26 12:26:14 -0500

    First commit.

---
+++ src/main/java/za/co/wethinkcode/Main.java
@@ -0,0 +1,15 @@
+package za.co.wethinkcode;
+
+import za.co.wethinkcode.flow.*;
+
+public class Main {
+
+    static {
+        new Recorder().logRun();
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/za/co/wethinkcode/MainTest.java
@@ -0,0 +1,12 @@
+package za.co.wethinkcode;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertEquals(true,true);
+    }
+}
